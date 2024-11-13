17f7455769dea0631133193efc40e7f0b724b750
Petrhonez <pfcandez01@gmail.com>
2024-11-13 16:23:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-document.getElementById('map').addEventListener('click', function (event) {
-    const mascot = document.getElementById('mascot');
-    
-    // Pegando as coordenadas clicadas
-    const x = event.clientX - this.getBoundingClientRect().left - (mascot.clientWidth / 2);
-    const y = event.clientY - this.getBoundingClientRect().top - (mascot.clientHeight / 2);
+document.addEventListener('mousemove', (event) => {
+    const horizontalLine = document.getElementById('horizontal-line');
+    const verticalLine = document.getElementById('vertical-line');
+    const customCursor = document.getElementById('custom-cursor');
 
-    // Movendo o mascote para as coordenadas clicadas
-    mascot.style.left = `${x}px`;
-    mascot.style.top = `${y}px`;
+    horizontalLine.style.top = `${event.clientY}px`;
+    verticalLine.style.left = `${event.clientX}px`;
+    customCursor.style.top = `${event.clientY}px`;
+    customCursor.style.left = `${event.clientX}px`;
 });
-
